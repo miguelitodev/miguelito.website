65a5034624982781e714e40c157e9044b7ae05db
Miguel Riquelme <miguelrisquelme@gmail.com>
2023-11-01 10:13:57 -0300

    MW-3 | Adjusting the initial structure of website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <Image className='' src="/img/hamster-loading-coming.gif" width={300} height={300} alt="Hamster doing things" />
+      <Image loading='lazy' placeholder='blur' blurDataURL='/img/hamster-loading.jpg' src="/img/hamster-loading-coming.gif" width={300} height={300} alt="Hamster doing things" />
       <h1 className='mt-3 font-sans text-3xl antialiased font-bold tracking-wide'>Coming soon</h1>
     </main>
   )
